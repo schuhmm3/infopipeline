44043287fb246d4adcdebfd158684d2b321178f8
pcrabarie <pcrabarie@paradigmadigital.com>
2020-11-13 19:25:05 +0100

    init

---
+++ src/main/java/com/basf/infopipeline/InfoPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.basf.infopipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basf/infopipeline/InfopipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basf.infopipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfopipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
