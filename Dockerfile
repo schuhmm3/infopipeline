4a59c326711cecb867eb3b9b2725fad26178f75c
pcrabarie <pcrabarie@paradigmadigital.com>
2020-11-16 21:28:43 +0100

    refactor mongodb connection

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM adoptopenjdk/openjdk11
+EXPOSE 8080
+ARG JAR_FILE
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/basf/infopipeline/MongoConfig.java
+++ src/main/java/com/basf/infopipeline/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -12,14 +13,17 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.uri}")
+    String uri;
+
     @Override
     protected String getDatabaseName() {
-        return "test";
+        return "infopipeline";
     }
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+        ConnectionString connectionString = new ConnectionString(uri);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .build();
